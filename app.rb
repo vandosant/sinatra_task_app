207b6b616b162f16f341318edb04c0e856ab7fac
vandosant <sjskender@gmail.com>
2014-03-27 08:22:04 -0600

    Users can create an item

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
 require 'sinatra/base'
 
 class App < Sinatra::Application
+  ITEMS = []
+
   get '/' do
-    "welcome"
+    erb :index
+  end
+
+  get '/new' do
+    erb :new
+  end
+
+  post '/' do
+    ITEMS << params[:new_item]
+    redirect '/'
   end
 end
--- spec/items_spec.rb
+++ spec/items_spec.rb
@@ -4,9 +4,13 @@ require_relative '../app'
 Capybara.app = App
 
 feature "item management" do
-  scenario "users can visit homepage" do
+  scenario "users can create an item" do
     visit '/'
 
-    expect(page).to have_content "welcome"
+    click_link "New Item"
+    fill_in "new_item", with: "Pizza"
+    click_on "Create Item"
+
+    expect(page).to have_content "Pizza"
   end
 end
